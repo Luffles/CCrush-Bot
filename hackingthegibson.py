b714f09326d5ef20d6d9a140e073a7b3ad3d9492
Alan Johnston <johnstonalanl@gmail.com>
2018-11-09 08:27:00 -0500

    no message im a hacker lol

---
+++ hackingthegibson.py
@@ -0,0 +1,11 @@
+def main():
+    counter = 0
+    print('im printing!')
+    while True:
+        print(counter)
+        counter += 1
+        if counter >= 69:
+            break
+
+if __name__ == '__main__':
+    main()
